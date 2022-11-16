89ae898288eff7d51000a5aea20023dc0082899d
jossi <rolety2202@naver.com>
2022-11-17 01:45:03 +0900

    -배포-

--- src/test/java/com/lotto/lotto_simulator/LottoSimulatorApplicationTests.java
+++ src/test/java/com/lotto/lotto_simulator/LottoSimulatorApplicationTests.java
@@ -1,13 +1,13 @@
-package com.lotto.lotto_simulator;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class LottoSimulatorApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.lotto.lotto_simulator;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class LottoSimulatorApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}

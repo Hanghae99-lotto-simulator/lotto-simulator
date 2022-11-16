292ece3c7599c2c5267da9fb2e77be5039320439
kidonge <ehdgus3519@naver.com>
2022-11-16 23:28:49 +0900

    refactor: 반환타입을 ResponseDto 수정, @CrossOrigin 추가
    LottoService 및 Lottocontroller의 반환 타입을 ResponseDto로 수정
    LottoController에@CrossOrigin 어노테이션을 붙여 Cors에러 해결
    
    issue: #3

--- src/main/java/com/lotto/lotto_simulator/controller/LottoController.java
+++ src/main/java/com/lotto/lotto_simulator/controller/LottoController.java
@@ -1,11 +1,10 @@
 package com.lotto.lotto_simulator.controller;
 
+import com.lotto.lotto_simulator.controller.requestDto.LottoDto;
+import com.lotto.lotto_simulator.controller.responseDto.ResponseDto;
 import com.lotto.lotto_simulator.service.LottoService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -13,19 +12,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class LottoController {
     private final LottoService lottoService;
     //로또 더미데이터 여러개
-    @PostMapping("/lottoAuto/{nums}")
-    public void lottoAutos(@PathVariable Long nums){
-        lottoService.lottoCreates(nums);
+    @CrossOrigin
+    @PostMapping("/lottoAutos/{nums}")
+    public ResponseDto<?> lottoAutos(@PathVariable Long nums){
+        return lottoService.lottoCreates(nums);
     }
     //로또 한개 자동
-    @PostMapping("/lottoAuto/1")
-    public void lottoAuto(){
-        lottoService.lottoCreate();
+    @CrossOrigin
+    @PostMapping("/lottoAutos")
+    public ResponseDto<?> lottoAuto() {
+        return lottoService.lottoCreate();
     }
 
+    @CrossOrigin
+    @PostMapping("/lottoManuals")
+    public ResponseDto<?> lottoManual(@RequestBody LottoDto lottoDto){
+        return lottoService.lottoManual(lottoDto);
+    }
+
+    // {num} 회차일 때 더미데이터 중 1등부터 5등까지 각각 몇명인지 반환
+    @CrossOrigin
     @PostMapping("/lottoWins/{num}")
-    public void lottoWin(@PathVariable Long num){
-        lottoService.lottoWins(num);
+    public ResponseDto<?> lottoWin(@PathVariable Long num){
+        return lottoService.lottoWins(num);
     }
 
 }

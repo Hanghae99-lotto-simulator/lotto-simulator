73a75710f8c9cdb2561285808eae04ca191c6edd
mookid214 <mookid214@gmail.com>
2022-11-29 10:04:38 +0900

    feature: RoundWinnersRepository 추가
    RoundWinnersRepository를 추가 했는데 이것도 쿼리dsl 형식으로 해야하나요?
    
    issue: #43

---
+++ src/main/java/com/lotto/lotto_simulator/repository/roundwinnersrepository/RoundWinnersRepository.java
@@ -0,0 +1,7 @@
+package com.lotto.lotto_simulator.repository.roundwinnersrepository;
+
+import com.lotto.lotto_simulator.entity.RoundWinners;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoundWinnersRepository extends JpaRepository<RoundWinners, Long> {
+}

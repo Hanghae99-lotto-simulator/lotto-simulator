92ac9a654ed775e46baae22ee378105bda1e0a5d
dongdongKing <ultraraptor@naver.com>
2022-12-03 14:48:44 +0900

    feat : 코드리뷰

--- src/test/java/com/lotto/lotto_simulator/LottoSimulatorApplicationTests.java
+++ src/test/java/com/lotto/lotto_simulator/LottoSimulatorApplicationTests.java
@@ -3,11 +3,14 @@ package com.lotto.lotto_simulator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.fail;
+
+//@SpringBootTest
 class LottoSimulatorApplicationTests {
 
     @Test
     void contextLoads() {
+        fail();
     }
 
 }

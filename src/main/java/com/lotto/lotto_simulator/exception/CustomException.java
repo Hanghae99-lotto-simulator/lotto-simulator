f8a9eb2c745cd1a67fb22616dae654b73af7a6b9
kidonge <ehdgus3519@naver.com>
2022-12-08 00:34:49 +0900

    feat: CustomException Lombok 어노테이션으로 변경
    기존에 있던 생성자 대신 Lombok 어노테이션으로 대체
    
    issue: #88

--- src/main/java/com/lotto/lotto_simulator/exception/CustomException.java
+++ src/main/java/com/lotto/lotto_simulator/exception/CustomException.java
@@ -1,14 +1,11 @@
 package com.lotto.lotto_simulator.exception;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class CustomException extends RuntimeException {
     private final CustomError customError;
-
-    public CustomException( CustomError customError){
-//        this.customError.getMessage());
-        this.customError = customError;
-    }
 }
 

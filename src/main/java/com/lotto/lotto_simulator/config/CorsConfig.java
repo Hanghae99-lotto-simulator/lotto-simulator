70b19d07c500e1921058eeb43b7a6c48ef18b4f1
jossi <rolety2202@naver.com>
2022-12-12 16:06:27 +0900

    hotfix : 코스오리진
    프론트 코스 오류 수정

--- src/main/java/com/lotto/lotto_simulator/config/CorsConfig.java
+++ src/main/java/com/lotto/lotto_simulator/config/CorsConfig.java
@@ -12,6 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("https://lotto.kidonge.shop/")
+                .allowedOrigins("http://localhost:3000/")
                 .allowedMethods("GET", "POST");
     }
 }
